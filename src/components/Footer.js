b5f8f080ffbebc9be3c1bf80dddd1adf36577759
Sergio Camacho <sergioa.camachoc@gmail.com>
2022-05-26 11:38:33 -0500

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por Sergio Camacho
+    </div>
+    <div>
+      <a href="https://www.utadeo.edu.co/es">Universidad Jorge Tadeo Lozano</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
